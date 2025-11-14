f4fb5ea77d2ab2968c52f0bd28daa78dd12228d8
Sergey Malyuga <sergey@malyuga.ru>
2025-11-14 15:45:53 +0300

    Создать catalog.component. Завершить вёрстку catalog.component

---
+++ src/app/features/catalog/catalog.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  imports: [],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CatalogComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
+import { CatalogComponent } from '../../features/catalog/catalog.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, CatalogComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
