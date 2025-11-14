2b53a5914efeb3a84c12951a14f1b2a135b23272
Sergey Malyuga <sergey@malyuga.ru>
2025-11-14 16:11:24 +0300

    Создать order.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { CatalogComponent } from '../../features/catalog/catalog.component';
+import { OrderComponent } from '../../features/order/order.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, CatalogComponent],
+  imports: [HeaderComponent, HeroComponent, CatalogComponent, OrderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
