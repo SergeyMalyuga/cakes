1d97f5145a99d64c7b1006769bc1e00d84293592
Sergey Malyuga <sergey@malyuga.ru>
2025-11-16 13:25:03 +0300

    Создать about.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { CatalogComponent } from '../../features/catalog/catalog.component';
 import { OrderComponent } from '../../features/order/order.component';
+import {AboutComponent} from "../../features/about/about.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, CatalogComponent, OrderComponent],
+    imports: [HeaderComponent, HeroComponent, CatalogComponent, OrderComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
