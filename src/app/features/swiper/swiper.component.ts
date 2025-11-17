2db1514ae8fa67c5dde7bd7afe416cc35ecfc8b1
Sergey Malyuga <sergey@malyuga.ru>
2025-11-17 21:07:19 +0300

    Настроить отображение слайдера при разном разрешении.

--- src/app/features/swiper/swiper.component.ts
+++ src/app/features/swiper/swiper.component.ts
@@ -1,41 +1,48 @@
 import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  inject,
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    inject,
 } from '@angular/core';
 import Swiper from 'swiper';
-import { Autoplay, Pagination, Navigation, EffectFlip } from 'swiper/modules';
+import {Autoplay, Pagination, Navigation, EffectFlip} from 'swiper/modules';
 
 @Component({
-  selector: 'app-swiper',
-  imports: [],
-  templateUrl: './swiper.component.html',
-  styleUrl: './swiper.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-swiper',
+    imports: [],
+    templateUrl: './swiper.component.html',
+    styleUrl: './swiper.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SwiperComponent implements AfterViewInit {
-  private swiper!: Swiper;
-  private elementRef: ElementRef = inject(ElementRef);
+    private swiper!: Swiper;
+    private elementRef: ElementRef = inject(ElementRef);
 
-  ngAfterViewInit() {
-    this.swiper = new Swiper(this.elementRef.nativeElement.querySelector('.swiper'), {
-      modules: [Autoplay, Pagination, Navigation, EffectFlip],
-      slidesPerView: 1,
-      autoplay: {
-        delay: 3000,
-      },
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.reviews__button-next',
-        prevEl: '.reviews__button-prev',
-      },
-    });
-  }
+    ngAfterViewInit() {
+        this.swiper = new Swiper(this.elementRef.nativeElement.querySelector('.swiper'), {
+            modules: [Autoplay, Pagination, Navigation, EffectFlip],
+            centeredSlides: true,
+            watchOverflow: true,
+            spaceBetween: 1,
+            navigation: {
+                nextEl: '.reviews__button-next',
+                prevEl: '.reviews__button-prev',
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                    spaceBetween: 35
+                },
+                1400: {
+                    slidesPerView: 1.75,
+                    spaceBetween: 35
+                },
+                1920: {
+                    slidesPerView: 1,
+                    spaceBetween: 35
+                }
+            },
+        });
+    }
 }
