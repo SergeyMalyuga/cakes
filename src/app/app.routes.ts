f74a0c39639bdbb857ae1a94ab1a076033007a17
Sergey Malyuga <sergey@malyuga.ru>
2025-11-19 10:44:31 +0300

    Создать footer.component. Завершить вёрстку footer.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AppRoute } from './core/constants/const';
 export const routes: Routes = [
   {
     path: AppRoute.MAIN,
-    title: 'Main',
+    title: 'Сладкий сундук',
     loadComponent: () => import('./pages/main/main.component').then((m) => m.MainComponent),
   },
 ];
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,19 +7,21 @@ import {AboutComponent} from "../../features/about/about.component";
 import { ReviewsComponent } from '../../features/reviews/reviews.component';
 import { ExamplesComponent } from '../../features/examples/examples.component';
 import { MakeOrderComponent } from '../../features/make-order/make-order.component';
+import {FooterComponent} from "../../features/footer/footer.component";
 
 @Component({
   selector: 'app-main',
-  imports: [
-    HeaderComponent,
-    HeroComponent,
-    CatalogComponent,
-    OrderComponent,
-    AboutComponent,
-    ReviewsComponent,
-    ExamplesComponent,
-    MakeOrderComponent,
-  ],
+    imports: [
+        HeaderComponent,
+        HeroComponent,
+        CatalogComponent,
+        OrderComponent,
+        AboutComponent,
+        ReviewsComponent,
+        ExamplesComponent,
+        MakeOrderComponent,
+        FooterComponent,
+    ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
